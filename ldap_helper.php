64fc91fa226ac28e9dc58d64e02b3b6180113b62
Matthew Barraud <matt@yachtmanager.co.uk>
2020-10-09 08:20:20 +0100

    Update 1

--- ldap_helper.php
+++ ldap_helper.php
@@ -1,13 +1,20 @@
 <?php
 
-  function ldapLogin($username, $password) {
+  //LDAP params
+  define('LDAP_SERVER', '');
+  define('LDAP_DOMAIN', '');
+  define('LDAP_PORT', 389);
+  define('LDAP_DN', '');
+  define('LDAP_USER', '');
+  define('LDAP_PSWD', '');
 
-    //make a connection to the LDAP server
+  function ldapLogin() {
+
+	 //make a connection to the LDAP server
     $connection = ldap_connect(LDAP_SERVER, LDAP_PORT);
     if (!$connection) {
       $result = 'false, Could not connect to the LDAP Server';
       return $result;
-      exit;
     }
 
     //set LDAP protopcol version etc.
@@ -15,17 +22,15 @@
     ldap_set_option($connection , LDAP_OPT_REFERRALS, 0);
 
     //Send credentials
-    $bind = @ldap_bind($connection, $username.LDAP_DOMAIN, $password);
+    $bind = @ldap_bind($connection, LDAP_USER.LDAP_DOMAIN, LDAP_PSWD);
     if (!$bind) {
       $result = 'false, Your credentials were incorrect';
       return $result;
-      exit;
     }
 
-    var_dump($bind);
-
     //Set filter attributes
-    $filter = "(sAMAccountName=" . $username . ")";
+	$user = substr(strrchr($_SERVER['AUTH_USER'], '\\'), 1); //Get the logged in username
+    $filter = "(sAMAccountName=" . $user . ")";
     $attr = array("memberof", "cn", "givenname", "sn");
 
     //Do the search
@@ -33,7 +38,6 @@
     if (!$search) {
       $result = 'false, Unable to search the LDAP Server';
       return $result;
-      exit;
     }
 
     //Iterate through the entries and check membership
@@ -49,24 +53,27 @@
       //Member of Intranet Admins
       if (strpos($commonNames, 'IntranetAdmin')) {
         $_SESSION['userGroup'] = 'AdminGroup';
-        $_SESSION['userFullName'] = $entries[0]['givenname'][0];
+        $_SESSION['userFirstName'] = $entries[0]['givenname'][0];
+		$_SESSION['userFullName'] = $entries[0]['givenname'][0].' '.$entries[0]['sn'][0];
         $result = 'true, Authenticated';
         return $result;
-        exit;
       }
 
       //Member of Intranet Users
       if (strpos($commonNames, 'IntranetUsers')) {
         $_SESSION['userGroup'] = 'UsersGroup';
-        $_SESSION['userFullName'] = $entries[0]['givenname'][0];
+        $_SESSION['userFirstName'] = $entries[0]['givenname'][0];
+		$_SESSION['userFullName'] = $entries[0]['givenname'][0].' '.$entries[0]['sn'][0];
         $result = 'true, You have been successfully logged in';
         return $result;
-        exit;
       }
+
+	  $_SESSION['userGroup'] = 'NoGroup';
+	  $_SESSION['userFirstName'] = $entries[0]['givenname'][0];
+	  $_SESSION['userFullName'] = $entries[0]['givenname'][0].' '.$entries[0]['sn'][0];
     }
 
     //We got this far, no idea how; send false anyway
     $result = 'false, An unknown error occured';
     return $result;
-    exit;
   }
